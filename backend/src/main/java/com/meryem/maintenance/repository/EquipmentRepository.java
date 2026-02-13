535d3d842caa9182ecc5feda78c4cae9245acd0e
Khalid kiaa <kiaakhalid4@gmail.com>
2026-02-13 07:41:12 +0100

    test: enhance test coverage for backend

---
+++ backend/src/main/java/com/meryem/maintenance/repository/EquipmentRepository.java
@@ -0,0 +1,9 @@
+package com.meryem.maintenance.repository;
+
+import com.meryem.maintenance.entity.Equipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
+}
